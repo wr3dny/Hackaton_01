e35e7787e834d12ff69e9f563c5967064eaf83f2
wr3dny <you@example.com>
2022-05-28 17:08:24 +0200

    name generator done

--- RPG_Heroe_name_generator.py
+++ RPG_Heroe_name_generator.py
@@ -1,36 +1,69 @@
-# Lets beggin with RPG Heroe name
-# Generator imienia dla Wojownika RPG:
-#
-#     Konieczność użycia modułu random.
-#     Program generuje wymawialne(!) imię o zadanej długości i dodaje do niego przydomek
-#     (opcjonalnie również tytuł i liczebnik). Np. 'Jouxdrien II Niemrawy'.
-#     Pomysł jest zainspirowany grą: http://progressquest.com/play/roster.html
-#     Imię musi zaczynać się od wielkiej litery.
-#     Program można kontynuować używając pomysłu poniżej.
-
 import random
+from time import sleep
+sleep_value = 2
 
-name_parts = ['ab', 'ce', 'on', 'lu', 'xu', 'pi', 'ur', 'os', 'ut', 'zo', 'mi', 'wa']
+user_name_lenght = int(input("Give Your heroe name lenght (please use even number): "))
+user_aligmnet = input('Are You "good" or "bad": ')
 
-generated_name = []
+vowel = ['a', 'e', 'i', 'o', 'u']
+consonant = ['b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'n', 'q', 's', 't', 'v',
+             'x', 'z', 'h', 'r', 'w', 'y']
+name_parts = []
+i = 0
+while i <= 105:
+    vr = random.choice(vowel)
+    cr = random.choice(consonant)
+    syllables = cr + vr
+    name_parts.append(syllables)
+    i += 1
+    name_parts = list(set(name_parts))
 
-a = random.choice(name_parts)
-b = random.choice(name_parts)
-c = random.choice(name_parts)
-d = random.choice(name_parts)
+def name_generator(name_lenght):
+    generated_name = []
+    if abs(name_lenght % 2 != 0):
+            print('You picked and odd number')
+            name_length = int(input('Pick up Your number again: '))
+    else:
+        i = 0
+        for i in range(0, int(name_lenght/2)):
+            generated_name.append(random.choice(name_parts))
+            i += 1
 
-generated_name.append(a)
-generated_name.append(b)
-generated_name.append(c)
-generated_name.append(d)
+    name_out: str = ' '.join(generated_name)
+    name_out = name_out.replace(' ', '')
+    name_out = name_out.capitalize()
+    return name_out
 
-print(generated_name)
+def nickname_generator(alignment):
+    while True:
+        if alignment == 'good':
+            nickname_good = ['Handsome', 'Bold', 'Big', 'Swift', 'Pure', 'Fair']
+            nickname = random.choice(nickname_good)
+        elif alignment == 'bad':
+            nickname_bad = ['Decayed', 'Sinister', 'Oathbreaker', 'Unpleasant', 'Blackheart', 'Wicked']
+            nickname = random.choice(nickname_bad)
+        #     break
+        # else:
+        #     print('You picked up wrong')
+        return nickname
 
-name_out = ' '.join(generated_name)
-name_out = name_out.replace(' ', '')
-name_out = name_out.capitalize()
-print(name_out)
 
-nickname_base = ['Handsome', 'Bold', 'Decayed', 'Lazy', 'Oathbreaker', 'Unpleasant', 'Big', 'Necromancer', 'Blackheart']
-nickname = random.choice(nickname_base)
-print(f'Your heroe is called {name_out} the {nickname}')
+name_out = name_generator(user_name_lenght)
+nickname = nickname_generator(user_aligmnet)
+
+if name_out[-1] == 'a':
+    he_she = 'she'
+else:
+    he_she = 'he'
+
+# def story(how_long):
+#     x = 0
+#     if x in range(0, how_long):
+
+
+print('Your heroe is created')
+sleep(sleep_value)
+print(f'{he_she.capitalize()} is called {name_out} the {nickname}')
+sleep(sleep_value)
+
+our_heroe = name_out + ' the ' + nickname
