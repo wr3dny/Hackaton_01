6d7a39a840ef74182c2abef879132e14770bc1e7
wr3dny <you@example.com>
2022-05-28 13:16:21 +0200

    building up tension and adding lines to code

--- RPG_story_draft.py
+++ RPG_story_draft.py
@@ -8,6 +8,7 @@
 #     Historyjka ma mieć zadaną długość i ma być możliwie losowa.
 
 import random
+from time import sleep
 
 vowel = ['a', 'e', 'i', 'o', 'u']
 consonant = ['b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'n', 'q', 's', 't', 'v', 'x', 'z', 'h', 'r', 'w', 'y']
@@ -37,38 +38,39 @@ our_heroe = str(name_out + ' the ' + nickname)
 print(f'Your heroe is called {our_heroe}')
 print()
 
-movement_to_happen = ['moved 20 paces north', 'moved 30 paces south', 'shouted at nearest person', 'died']
-actions_to_happen = ['took a big breath', 'spited out', 'attacked wolverine', 'attacked pixie']
-destinations_to_meet = ['inn', 'old castle', 'dwarven mine', 'town square']
-
-actions = random.choice(actions_to_happen)
-movement = random.choice(movement_to_happen)
-destinations = random.choice(destinations_to_meet)
-
-print(name_out)
-
 if name_out[-1] == a:
     he_she = 'she'
 else:
     he_she = 'he'
 
+distance = random.randrange(0, 500, 10)
+possible_directions = ['north', 'south', 'east', 'west', 'northeast', 'northwest', 'southeast', 'southwest']
+actions_to_happen = ['took a big breath', 'spited out', 'attacked wolverine', 'attacked pixie',
+                     'shouted at nearest person', 'fall a sleep', 'brawled with NPC', ]
+#attacked_things = ['pixie', 'wolverine','wherewolf', ']
+possible_places= ['inn', 'old castle', 'deep dwarven mine', 'town square', 'shadowy forrest', 'wide road',
+                  'godforsaken village', 'seashore']
+
+action = random.choice(actions_to_happen)
+direction = random.choice(possible_directions)
+places = random.choice(possible_places)
+distance = str(distance) + ' paces'
 
 print(f'So our story begins when {our_heroe} stepped outside the tavern "Saturday\'s Hangover".')
-print(f'{he_she.capitalize()} looked around {movement} and {actions}.')
+sleep(1)
+print(f'{he_she.capitalize()} looked around {action} and {action}.')
+sleep(1)
+action = random.choice(actions_to_happen)
+direction = random.choice(possible_directions)
+places = random.choice(possible_places)
+distance = str(distance) + ' paces'
+print(f'After that {he_she} {action} and moved {direction} .')
+sleep(1)
+action = random.choice(actions_to_happen)
+direction = random.choice(possible_directions)
+places = random.choice(possible_places)
+distance = str(distance) + ' paces'
 
-movement_to_happen = ['moved 20 paces north', 'moved 30 paces south']
-actions_to_happen = ['took a big breath', 'spited out', 'attacked wolverine', 'attacked pixie',
-                     'shouted at nearest person', 'died']
-destinations_to_meet = ['inn', 'old castle', 'dwarven mine', 'town square']
-
-actions = random.choice(actions_to_happen)
-movement = random.choice(movement_to_happen)
-destinations = random.choice(destinations_to_meet)
-
-print(f'After that {he_she} {actions} and {movement}.')
-
-actions = random.choice(actions_to_happen)
-movement = random.choice(movement_to_happen)
-destinations = random.choice(destinations_to_meet)
-
-print(f'{destinations.capitalize()} was opportunity for some coins, so {he_she} {actions} but first {he_she} had to {movement}')
+print(f'{places.capitalize()} was opportunity for some coins, so {he_she} {action} but first {he_she} '
+      f'had to move {distance} {direction}')
+sleep(1)
