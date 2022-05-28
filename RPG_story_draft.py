1a06ea094a6d3084a1bcf0c5e887c06ba2aec56c
wr3dny <you@example.com>
2022-05-28 14:16:12 +0200

    working on

--- RPG_story_draft.py
+++ RPG_story_draft.py
@@ -10,10 +10,20 @@
 import random
 from time import sleep
 
-vowel = ['a', 'e', 'i', 'o', 'u']
-consonant = ['b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'n', 'q', 's', 't', 'v', 'x', 'z', 'h', 'r', 'w', 'y']
+def name_parts(consonant, vowel):
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    consonant = ['b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'n', 'q', 's', 't', 'v',
+                 'x', 'z', 'h', 'r', 'w', 'y']
+    name_parts = []
+    i = 0
+    while i <= 105:
+        vr = random.choice(vowel)
+        cr = random.choice(consonant)
+        syllables = cr + vr
+        name_parts.append(syllables)
+        i += 1
+    name_parts = list(set(name_parts))
 
-name_parts = ['bo', 'ce', 'na', 'lu', 'xu', 'pi', 'ra', 'sa', 'ut', 'zo', 'mi', 'wa']
 
 generated_name = []
 
@@ -38,39 +48,84 @@ our_heroe = str(name_out + ' the ' + nickname)
 print(f'Your heroe is called {our_heroe}')
 print()
 
-if name_out[-1] == a:
+if name_out[-1] == 'a':
     he_she = 'she'
 else:
     he_she = 'he'
 
-distance = random.randrange(0, 500, 10)
+distance = random.randrange(1, 100)
 possible_directions = ['north', 'south', 'east', 'west', 'northeast', 'northwest', 'southeast', 'southwest']
 actions_to_happen = ['took a big breath', 'spited out', 'attacked wolverine', 'attacked pixie',
                      'shouted at nearest person', 'fall a sleep', 'brawled with NPC', ]
-#attacked_things = ['pixie', 'wolverine','wherewolf', ']
-possible_places= ['inn', 'old castle', 'deep dwarven mine', 'town square', 'shadowy forrest', 'wide road',
+# attacked_things = ['pixie', 'wolverine','wherewolf','skeleton', 'zombie', 'chaos dwarf', 'nurgle spawn', 'goblin',
+# 'human bodyguard', 'hangman', 'tax collector']
+possible_place = ['inn', 'old castle', 'deep dwarven mine', 'town square', 'shadowy forrest', 'wide road',
                   'godforsaken village', 'seashore']
 
 action = random.choice(actions_to_happen)
 direction = random.choice(possible_directions)
-places = random.choice(possible_places)
+place = random.choice(possible_place)
 distance = str(distance) + ' paces'
 
 print(f'So our story begins when {our_heroe} stepped outside the tavern "Saturday\'s Hangover".')
-sleep(1)
-print(f'{he_she.capitalize()} looked around {action} and {action}.')
-sleep(1)
+sleep(2)
+print(f'{he_she.capitalize()} looked around {random.choice(actions_to_happen)} and {random.choice(actions_to_happen)}.')
+sleep(2)
 action = random.choice(actions_to_happen)
 direction = random.choice(possible_directions)
-places = random.choice(possible_places)
+place = random.choice(possible_place)
 distance = str(distance) + ' paces'
 print(f'After that {he_she} {action} and moved {direction} .')
-sleep(1)
+sleep(2)
 action = random.choice(actions_to_happen)
 direction = random.choice(possible_directions)
-places = random.choice(possible_places)
+place = random.choice(possible_place)
 distance = str(distance) + ' paces'
 
-print(f'{places.capitalize()} was opportunity for some coins, so {he_she} {action} but first {he_she} '
+print(f'{place.capitalize()} was opportunity for some coins, so {he_she} {action} , then {he_she} '
       f'had to move {distance} {direction}')
-sleep(1)
+sleep(2)
+action = random.choice(actions_to_happen)
+direction = random.choice(possible_directions)
+place = random.choice(possible_place)
+distance = str(distance) + ' paces'
+
+print(f'The {place} was opportunity for some coins, so {he_she} {action} , then {he_she} '
+      f'had to move {distance} {direction}')
+sleep(2)
+action = random.choice(actions_to_happen)
+direction = random.choice(possible_directions)
+place = random.choice(possible_place)
+distance = str(distance) + ' paces'
+
+print(f'The {place} was opportunity for some coins, so {he_she} {action} , then {he_she} '
+      f'had to move {distance} {direction}')
+sleep(2)
+action = random.choice(actions_to_happen)
+direction = random.choice(possible_directions)
+place = random.choice(possible_place)
+distance = str(distance) + ' paces'
+
+print(f'The {place} was opportunity for some coins, so {he_she} {action} , then {he_she} '
+      f'had to move {distance} {direction}')
+sleep(2)
+action = random.choice(actions_to_happen)
+direction = random.choice(possible_directions)
+place = random.choice(possible_place)
+distance = str(distance) + ' paces'
+
+print(f'The {place} was opportunity for some coins, so {he_she} {action} , then {he_she} '
+      f'had to move {distance} {direction}')
+sleep(2)
+action = random.choice(actions_to_happen)
+direction = random.choice(possible_directions)
+place = random.choice(possible_place)
+distance = str(distance) + ' paces'
+
+# def randomized (all):
+#     action = random.choice(actions_to_happen)
+#     direction = random.choice(possible_directions)
+#     place = random.choice(possible_place)
+#     distance = random.randrange(1, 100)
+#     distance = str(distance) + ' paces'
+
