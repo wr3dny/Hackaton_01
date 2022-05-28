48caa9b73229e05fbaf30dfa4c1d527e554ce709
wr3dny <you@example.com>
2022-05-28 11:28:00 +0200

    working on, making ? progress

--- lets_get_it_started.py
+++ lets_get_it_started.py
@@ -1,6 +1,35 @@
 # Lets beggin with RPG Heroe name
+# Generator imienia dla Wojownika RPG:
+#
+#     Konieczność użycia modułu random.
+#     Program generuje wymawialne(!) imię o zadanej długości i dodaje do niego przydomek
+#     (opcjonalnie również tytuł i liczebnik). Np. 'Jouxdrien II Niemrawy'.
+#     Pomysł jest zainspirowany grą: http://progressquest.com/play/roster.html
+#     Imię musi zaczynać się od wielkiej litery.
+#     Program można kontynuować używając pomysłu poniżej.
+
 
 import random
 
-avaible_syllables_for genereting_name = ['ab', 'ce', 'on', 'lu', 'xu']
-generated_name =
+name_parts = ['ab', 'ce', 'on', 'lu', 'xu', 'yun', 'mun', 'pi', 'ur', 'os', ]
+
+generated_name = []
+
+a = random.choice(name_parts)
+b = random.choice(name_parts)
+c = random.choice(name_parts)
+d = random.choice(name_parts)
+
+generated_name.append(a)
+generated_name.append(b)
+generated_name.append(c)
+generated_name.append(d)
+
+print(generated_name)
+
+name_out = ' '.join(generated_name)
+name_out = name_out.replace(' ', '')
+name_out = name_out.capitalize()
+print(name_out)
+
+
